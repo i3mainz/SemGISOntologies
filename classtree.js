7399862dcbf66e5a1d3b5a2b33bc6eddf5f06aa8
Timo Homburg <timo.homburg@hs-mainz.de>
2020-07-29 23:08:41 +0200

    Update classtree.js

--- classtree.js
+++ classtree.js
@@ -1,35 +1,83 @@
-var classTree={"plugins": ["search", "types","sort"],"search": {}, "core": { "data" :[]}}
-function generateClassTree(){
-	classTree["core"]["data"].push({ "id" : "http://www.opengis.net/ont/geosparql#Feature", "parent":"#", "text" : "Feature" })
-	classTree["core"]["data"].push({ "id" : "Enumeration", "parent":"http://www.opengis.net/ont/geosparql#Feature", "text" : "Enumeration" })
+function generateClassTree(titleatt,superatt,classOrProp){
+	classTree={"plugins": ["search", "types","sort","state","wholerow"],"search": {"case_sensitive": false,"show_only_matches": true}, "core": { "data" :[]}}
+    parentmap={}
+    if(titleatt=="class"){
+	    classTree["core"]["data"].push({ "id" : "http://www.w3.org/2002/07/owl#Thing", "icon" : "https://raw.githubusercontent.com/protegeproject/protege/master/protege-editor-owl/src/main/resources/Classes.gif", "parent":"#", "text" : "owl:Thing" })
+    }else if(titleatt=="data property"){
+		classTree["core"]["data"].push({ "id" : "http://www.w3.org/2002/07/owl#Thing", "icon" : "https://raw.githubusercontent.com/protegeproject/protege/master/protege-editor-owl/src/main/resources/OWLDatatypeProperty.gif", "parent":"#", "text" : "owl:Thing" })
+    }else if(titleatt=="named individual"){
+		//classTree["core"]["data"].push({ "id" : id, "parent":"#", "icon" : "https://raw.githubusercontent.com/protegeproject/protege/master/protege-editor-owl/src/main/resources/OWLIndividual.gif", "text" : textt })
+	}else{
+		classTree["core"]["data"].push({ "id" : "http://www.w3.org/2002/07/owl#Thing", "icon" : "https://raw.githubusercontent.com/protegeproject/protege/master/protege-editor-owl/src/main/resources/OWLObjectProperty.gif", "parent":"#", "text" : "owl:Thing" })
+	}
 	var counter=0;
-    var iframe = document.getElementById('ontview');
-    console.log(iframe.contentWindow.document.querySelector('h3 > sup[title="class"]'));
-    console.log($('#ontview').contents().find("html > body"))
-	$('#ontview').contents().find('html > body > div > h3 > sup[title="class"]').each(function() {
+	//console.log($('#ontview').contents())
+	//console.log($('#ontview').contents().find('.type-c'))
+	//console.log($('#ontview').contents().find(' h3 > sup[title="'+titleatt+'"]'))
+	$('#ontview').contents().find(' h3 > sup[title="'+titleatt+'"]').each(function() {
+		console.log($(this))
 		if(counter>0){
 		var id=$(this).parent().parent().attr("id");
-		console.log(id)
+		////console.log(id)
 		var parentcls="";
-		
+		////console.log("Superclasses")
 		if(!(id.startsWith("4"))){
-		$(this).parent().parent().children('dl').children('dt:contains("has super-classes")').next().children("a").each(function(){
+			sup=$(this).parent().parent().children('dl').children('dt:contains("'+superatt+'")').next().children("a")
+			if(sup.length!=0){
+			$(this).parent().parent().children('dl').children('dt:contains("'+superatt+'")').next().children("a").each(function(){
+			//console.log($(this))
 			if(!($(this).attr("href").startsWith("4"))){
-				parentcls=$(this).attr("href").substring(1)
-				console.log($(this).attr("href"));
+				parentcls=$(this).attr("href").substring($(this).attr("href").indexOf('#')+1)
+				//console.log($(this).attr("href"));
 			}
 		});
+			}else{
+					$(this).parent().parent().children('div').children('dl').children('dt:contains("'+superatt+'")').next().children("a").each(function(){
+			//console.log($(this))
+			if(!($(this).attr("href").startsWith("4"))){
+				parentcls=$(this).attr("href").substring($(this).attr("href").indexOf('#')+1)
+				//console.log($(this).attr("href"));
+			}
+		});
+			}
+
 		if(parentcls==""){
 			parentcls="#"
 		}
 		
-		//console.log(superclasslist[0])
+		////console.log(superclasslist[0])
 		//var topush={ "id" : id,parent:
-		classTree["core"]["data"].push({ "id" : id, "parent":parentcls, "text" : id.substring(id.lastIndexOf('#')+1) })
+		if(id.includes('#')){
+			var textt=id.substring(id.lastIndexOf('#')+1)
+		}else{
+			var textt=id.substring(id.lastIndexOf('/')+1)
+		}
+		
+		if(titleatt=="class"){
+	        classTree["core"]["data"].push({ "id" : id, "parent":parentcls,"icon" : "https://raw.githubusercontent.com/protegeproject/protege/master/protege-editor-owl/src/main/resources/Classes.gif", "text" : textt })
+        }else if(titleatt=="data property"){
+			classTree["core"]["data"].push({ "id" : id, "parent":parentcls,"icon" : "https://raw.githubusercontent.com/protegeproject/protege/master/protege-editor-owl/src/main/resources/OWLDatatypeProperty.gif", "text" : textt })
+		}else if(titleatt=="named individual"){
+		     if(!(parentcls in parentmap)){
+				if(parentcls.includes('#')){
+					var textt2=parentcls.substring(parentcls.lastIndexOf('#')+1)
+				}else{
+					var textt2=parentcls.substring(parentcls.lastIndexOf('/')+1)
+				}
+		        classTree["core"]["data"].push({ "id" : parentcls, "parent":"#","icon" : "https://raw.githubusercontent.com/protegeproject/protege/master/protege-editor-owl/src/main/resources/Classes.gif", "text" : textt2 })
+		        parentmap[parentcls]=true
+		    }
+			classTree["core"]["data"].push({ "id" : id, "parent":parentcls,"icon" : "https://raw.githubusercontent.com/protegeproject/protege/master/protege-editor-owl/src/main/resources/OWLIndividual.gif", "text" : textt })
+		}else{
+            classTree["core"]["data"].push({ "id" : id, "parent":parentcls,"icon" : "https://raw.githubusercontent.com/protegeproject/protege/master/protege-editor-owl/src/main/resources/OWLObjectProperty.gif", "text" : textt })
+        }
+		
+		//console.log(classTree["core"]["data"])
 		}
 		}
 		counter++;
 		
 	});
-	console.log(classTree)
+	//console.log(classTree)
+	return classTree;
 }
