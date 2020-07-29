01d9f4e7cfc29148fed9878d3038deda951ea6b9
Timo Homburg <timo.homburg@hs-mainz.de>
2020-07-29 23:12:46 +0200

    Add new file

---
+++ pages.js
@@ -0,0 +1,9 @@
+var onts={
+ "AAA6":"aaa/doc/index-en.html",
+ "XPlanung5":"xplanung/doc/index-en.html",
+ "INSPIRE4":"inspire/index-en.html",
+ "CityGML":"citygml/doc/index-en.html",
+ "XErleben":"xerleben/doc/index-en.html",
+ "XKatastrophenhilfe":"xkat/doc/index-en.html"
+}
+
