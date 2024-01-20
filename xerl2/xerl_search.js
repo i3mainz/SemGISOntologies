527b653f244a3346874c1bd8fccc3a6c5ccc7d00
situx <situx@users.noreply.github.com>
2024-01-20 18:41:07 +0000

    Deploying to gh-pages from @ i3mainz/SemGISOntologies@96d3ad709a1f051d88f0c12458337991873a272e 🚀

--- xerl2/xerl_search.js
+++
@@ -1,9 +0,0 @@
-var search={
-  "AbstractFeature": "http://www.xerleben.de/schema/2.0_1/nonns_AbstractFeature.html",
-  "AbstractObject": "http://www.xerleben.de/schema/2.0_1/nonns_AbstractObject.html",
-  "Curve": "http://www.xerleben.de/schema/2.0_1/nonns_Curve.html",
-  "Feature": "http://www.xerleben.de/schema/2.0_1/nonns_Feature.html",
-  "MultiPoint": "http://www.xerleben.de/schema/2.0_1/nonns_MultiPoint.html",
-  "String.Latin": "http://www.xerleben.de/schema/2.0_1/nonns_String.Latin.html",
-  "Surface": "http://www.xerleben.de/schema/2.0_1/nonns_Surface.html"
-}
